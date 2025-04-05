215d98f773827e11abbd6a1cb5133a369d016f86
smegmalicker47 <smegmalicker47@gmail.com>
2025-04-04 20:17:09 -0400

    ONE FUCKING SEMICOLON AHHHHH

--- BazaarMacro/index.js
+++ BazaarMacro/index.js
@@ -1,14 +1,16 @@
 import Config from './config'
 
+
+
+
+
 //this is NOT wh
 //okay so lets see here
 //this is an index file so it's good and important
 //time to code
 // * stretches fingers
 
-
-
 register('command', () => {
-    Config().getConfig().openGui()
+    Config.openGUI();
 
 }).setName("bam");
